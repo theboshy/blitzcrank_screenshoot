0f8be3e19a457f5ec1d7f92b3ed035006157f33d
theboshy <akumaokami10@gmail.com>
2017-09-25 15:30:45 -0500

    firstCommit

---
+++ src/blitzcrank/Blitzcrank.java
@@ -0,0 +1,50 @@
+package blitzcrank;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author theboshy
+ */
+public class Blitzcrank {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.awt.AWTException
+     * @throws java.lang.InterruptedException
+     */
+    public static void main(String[] args) throws AWTException, InterruptedException, IOException {
+        String stringfyCurrent = String.valueOf(System.currentTimeMillis());
+        String path = "{path}";
+        String name = "capture" + stringfyCurrent;
+        String type = ".jpg";
+        //--
+        Robot robotAwt = new Robot();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        File file = new File(path + name + type);
+        BufferedImage imagexd = robotAwt.createScreenCapture(new Rectangle(screenSize.width, screenSize.height));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(imagexd, "jpg", baos);
+        byte[] bytes = baos.toByteArray();
+
+        OutputStream out = new FileOutputStream(file);
+
+        out.write(bytes);
+
+    }
+
+}
